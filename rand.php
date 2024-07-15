4019fe5a1e975f2818759c54032d2ebb813c1404
maxisigrist <maxi_sigrist@hotmail.com>
2024-07-15 13:20:48 -0300

    commit

--- rand.php
+++ rand.php
@@ -4,6 +4,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $valor = rand(1, 5);
-if $valor 
 
+if ($valor == 1 || $valor == 3 || $valor == 5) {
+    echo "El número $valor es impar";
+} else {
+    echo "El número $valor es par";
+}
 ?>
