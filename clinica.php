adc263358ea32e282fffdb411981432266c91ca5
maxisigrist <maxi_sigrist@hotmail.com>
2024-07-24 13:22:28 -0300

    commit

--- clinica.php
+++ clinica.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//Definicion de pacientes
+// Definición de pacientes
 $aPacientes = array();
 $aPacientes[] = array(
     "dni" => "33.765.012",
@@ -26,39 +26,42 @@ $aPacientes[] = array(
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=}, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CLINICA</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
     <main class="container">
         <div class="row">
-            <div class="col12">
+            <div class="col-12">
                 <h1>Listado de pacientes</h1>
                 <table class="table table-hover border">
                     <thead>
-                        <th>DNI</th>
-                        <th>NOMBRE Y APELLIDO</th>
-                        <th>EDAD</th>
-                        <th>PESO</th>
-                        <th></th>
+                        <tr>
+                            <th>DNI</th>
+                            <th>NOMBRE Y APELLIDO</th>
+                            <th>EDAD</th>
+                            <th>PESO</th>
+                        </tr>
                     </thead>
-                    <TBODy>
-                        <?php foreach ($aPacientes as $aPacientes){ ?>
-                        <TR>
-                            <td><?php echo $aPacientes[$i]["dni"]; ?></td>
-                            <td><?php echo $aPacientes[$i]["nombre"]; ?></td>
-                            <td><?php echo $aPacientes[$i]["edad"]; ?></td>
-                            <td><?php echo $aPacientes[$i]["peso"]; ?></td>
-                            
-                        </TR>
-                        <?php } ?> 
-                    </TBODy>
+                    <tbody>
+                        <?php foreach ($aPacientes as $paciente) { ?>
+                            <tr>
+                                <td><?php echo $paciente["dni"]; ?></td>
+                                <td><?php echo $paciente["nombre"]; ?></td>
+                                <td><?php echo $paciente["edad"]; ?></td>
+                                <td><?php echo $paciente["peso"]; ?></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
                 </table>
             </div>
         </div>
     </main>
 </body>
+
 </html>
