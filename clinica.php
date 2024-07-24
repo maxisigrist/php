2056c1c9e6fd96f5d28eb9ee9931b3567231f717
maxisigrist <maxi_sigrist@hotmail.com>
2024-07-24 12:57:05 -0300

    commit

--- clinica.php
+++ clinica.php
@@ -29,13 +29,14 @@ $aPacientes[] = array(
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=}, initial-scale=1.0">
-    <title>Document</title>
+    <title>CLINICA</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <main class="container">
         <div class="row">
             <div class="col12">
+                <h1>Listado de pacientes</h1>
                 <table class="table table-hover border">
                     <thead>
                         <th>DNI</th>
