0cda1a38aa46573baf2a8b5a05503b54c613cc75
maxisigrist <maxi_sigrist@hotmail.com>
2024-07-24 14:08:22 -0300

    commit

---
+++ neto.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Definicion
+function calcularNeto($bruto){
+    
+}
+
+//Uso
+echo "El sueldo neto es $" .calcularNeto(750000);
+?>
