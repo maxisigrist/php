b82a44d03d329e6a13628b9622688218fdff9fe1
maxisigrist <maxi_sigrist@hotmail.com>
2024-07-30 13:47:07 -0300

    commit

--- formulario_datos_personales/resultado.php
+++ formulario_datos_personales/resultado.php
@@ -1,26 +1,27 @@
-
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-if($_POST){
-    $Nombre = $_POST["txtNombre"];
-    $Dni = $_POST["txtDni"];
-    $Telefono = $_POST["txtTelefono"];
-    $Edad = $_POST["txtEdad"];
+if ($_POST) {
+    $nombre = $_POST["txtNombre"];
+    $dni = $_POST["txtDni"];
+    $telefono = $_POST["txtTelefono"];
+    $edad = $_POST["txtEdad"];
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
     <main class="container">
         <div class="row">
@@ -54,4 +55,5 @@ if($_POST){
 
     </main>
 </body>
+
 </html>
