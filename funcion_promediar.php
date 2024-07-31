0b99b089270e2c4d5a5749d332b24ad00c5c22db
maxisigrist <maxi_sigrist@hotmail.com>
2024-07-31 13:40:15 -0300

    commit

--- funcion_promediar.php
+++ funcion_promediar.php
@@ -1,14 +1,21 @@
 <?php
-//definicion
-function promediar($aNumeros) {
-    $suma = 0;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function promediar($aNumeros){
+    $sumatoria = 0;
     foreach ($aNumeros as $numero) {
-        $suma += $numero;
+        $sumatoria += $numero; //$sumatoria = $sumatoria + $numero;
     }
-    return $suma / count($aNumeros);
+    return $sumatoria / count($aNumeros);
 }
 
+//Uso
+$aNotas = array(8, 4, 5, 3, 9, 1);
+$aSueldo = array(800.30, 400.87, 500.45, 300, 900.70, 100, 900, 1800);
 
-//uso
-$aNotas = array(8, 4, 5, 3, 9, 1,);
-echo "El promedio es: " . promediar($aNotas) . "<br>";
+$promedioNotas = promediar($aNotas);
+
+echo "La nota promedio es: $promedioNotas<br>";
+echo "El sueldo  promedio es: " . promediar($aSueldo);
