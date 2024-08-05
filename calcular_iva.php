96510fc7e8e7c78ea458ea5472e80ca6c7657805
maxisigrist <maxi_sigrist@hotmail.com>
2024-08-05 14:44:04 -0300

    commit

---
+++ calcular_iva.php
@@ -0,0 +1,102 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$iva = 21;
+$precioSinIva = 0;
+$precioConIva = 0;
+$ivaCantidad = 0;
+
+if ($_POST) {
+  
+    //Capturar los datos del formulario
+    $iva = $_POST["lstIva"];
+    $precioSinIva = $_POST["txtPrecioSinIva"];
+    $precioConIva = $_POST["txtPrecioConIva"];
+
+    if(($precioConIva == "" && $precioSinIva == "")||($precioConIva != "" && $precioSinIva != "")){
+        $precioConIva = 0;
+        $precioSinIva = 0;
+    }
+
+    //Dado un importe sin IVA, Precio con IVA = importe * (21/100+1)
+    if($precioSinIva != "" && $precioConIva == ""){
+        $precioConIva = $precioSinIva * ($iva/100+1); 
+        $ivaCantidad = $precioConIva - $precioSinIva;
+    }
+   
+    //Dado un importe con IVA, Precio sin IVA = importe / (21/100+1)
+    if($precioConIva != "" && $precioSinIva == ""){
+        $precioSinIva = $precioConIva / ($iva/100+1);
+        $ivaCantidad = $precioConIva - $precioSinIva;
+
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora de IVA</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1>Calculadora de IVA</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3 offset-2">
+                <form method="POST">
+                    <div class="pb-3">
+                        <label for="">IVA</label>
+                        <select name="lstIva" id="lstIva" class="form-control">
+                            <option value="10.5">10.5</option>
+                            <option value="21" selected>21</option>
+                        </select>
+                    </div>
+                    <div class="pb-3">
+                        <label for="">Precio sin IVA:</label>
+                        <input type="text" id="txtPrecioSinIva" name="txtPrecioSinIva" class="form-control">
+                    </div>
+                    <div class="pb-3">
+                        <label for="">Precio con IVA:</label>
+                        <input type="text" id="txtPrecioConIva" name="txtPrecioConIva" class="form-control">
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary">ENVIAR</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-5 pt-4">
+                <table class="table table-hover border">
+                    <tr>
+                        <th>IVA:</th>
+                        <td><?php echo $iva; ?>%</td>
+                    </tr>
+                    <tr>
+                        <th>Precio sin IVA:</th>
+                        <td>$ <?php echo number_format($precioSinIva, 2, ",", "."); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Precio con IVA:</th>
+                        <td>$ <?php echo number_format($precioConIva, 2, ",", "."); ?></td>
+                    </tr>
+                    <tr>
+                        <th>IVA cantidad:</th>
+                        <td>$ <?php echo number_format($ivaCantidad, 2, ",", "."); ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
